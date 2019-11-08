f9a40d7c877ea4c7a99abecdf9dfc7f9db422944
Thomas Guibert <thomas.guibert@gloot.com>
2019-11-08 09:42:22 +0100

    Updating pull to refresh default content

--- src/components/pulling-content.tsx
+++ src/components/pulling-content.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PullingContent = () => {
   return (
     <div>
-      <p>&#8615;&nbsp;&nbsp;Pull To Refresh&nbsp;&nbsp;&#8615;</p>
+      <p>&#8615;&nbsp;&nbsp;pull to refresh&nbsp;&nbsp;&#8615;</p>
     </div>
   );
 };
