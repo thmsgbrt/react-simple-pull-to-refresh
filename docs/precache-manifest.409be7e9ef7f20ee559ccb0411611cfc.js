698e552222263b2c3dff503cda29d4b4cb598347
Thomas Guibert <th.guibert@gmail.com>
2021-10-19 15:04:56 +0200

    Update demo

---
+++ docs/precache-manifest.409be7e9ef7f20ee559ccb0411611cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cce5cdd1b42157dd56b9b3b754bb31",
+    "url": "/react-simple-pull-to-refresh/index.html"
+  },
+  {
+    "revision": "c46260c2f8c7be219a0e",
+    "url": "/react-simple-pull-to-refresh/static/css/main.4ed805f0.chunk.css"
+  },
+  {
+    "revision": "4eb5ba97362c21187cdb",
+    "url": "/react-simple-pull-to-refresh/static/js/2.ce7fa3e7.chunk.js"
+  },
+  {
+    "revision": "c46260c2f8c7be219a0e",
+    "url": "/react-simple-pull-to-refresh/static/js/main.60884689.chunk.js"
+  },
+  {
+    "revision": "e74c4cb01b298f907401",
+    "url": "/react-simple-pull-to-refresh/static/js/runtime-main.dd9dbc43.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-simple-pull-to-refresh/precache-manifest.fef0e41f6f03ce2eeb9e823693d76cc6.js"
+  "/react-simple-pull-to-refresh/precache-manifest.409be7e9ef7f20ee559ccb0411611cfc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-simple-pull-to-refresh/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
